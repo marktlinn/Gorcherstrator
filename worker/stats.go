57d327be06a7261aac4c4f393785a6482cac6dab
marktlinn <marktlinn@gmail.com>
2024-04-03 22:43:33 +0200

    add: basic metrics/proc stats for workers

--- worker/stats.go
+++ worker/stats.go
@@ -5,7 +5,7 @@ import "github.com/c9s/goprocinfo/linux"
 // Stats represents pointers to all the Linux processes information required to provide metrics about containers running on the system.
 type Stats struct {
 	MemStats  *linux.MemInfo
-	CPUStats  *linux.CPUInfo
+	CPUStats  *linux.CPUStat
 	LoadStats *linux.LoadAvg
 	DiskStats *linux.Disk
 }
@@ -30,4 +30,36 @@ func (s *Stats) MemUsedPercent() uint64 {
 // Shows the total amount of memory used in KB.
 func (s *Stats) MemUsedKB() uint64 {
 	return s.MemStats.MemTotal - s.MemStats.MemAvailable
+}
+
+// DiskFree returns the total amount of Disk space is free to be used.
+func (s *Stats) DiskFree() uint64 {
+	return s.DiskStats.Free
+}
+
+// DiskUsed returns the total amount of Disk space that's is being used.
+func (s *Stats) DiskUsed() uint64 {
+	return s.DiskStats.Used
+}
+
+// DiskTotal returns the total amount of Disk space that there is, including that which is currently used and that which is free.
+func (s *Stats) DiskTotal() uint64 {
+	return s.DiskStats.All
+}
+
+// CpuUsage gives the total amount of CPU currently being used as a percentage of the overall CPU capacity.
+// The percentage is calucated as:
+//
+//	((Sum all states) - (sum of idle states)) / sum of all states
+func (s *Stats) CpuUsage() float64 {
+	idleStates := s.CPUStats.Idle + s.CPUStats.IOWait
+	nonIdleStates := s.CPUStats.User + s.CPUStats.Nice + s.CPUStats.Steal + s.CPUStats.System + s.CPUStats.SoftIRQ + s.CPUStats.IRQ
+
+	ttl := idleStates + nonIdleStates
+
+	if ttl == 0 {
+		return 0.00
+	}
+
+	return (float64(ttl) - float64(idleStates)/float64(ttl))
 }
